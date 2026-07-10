f27cbd693ba3fa63dbf6b5e7533e017216fe7e44
Your Name <latoyashinaver@gmail.com>
2026-07-09 23:43:13 -0400

    Automation has curves

--- fundamentals_practice.js
+++ fundamentals_practice.js
@@ -2,7 +2,7 @@ const person = {
     name: "Latoya S.",
     age: 42,
     learningAutomation: "true",
-    favorite_tools: ["Selenium", "Postman", "JMeter", "Playwright"],
+    favorite_tools: ["Selenium", "Postman", "JMeter", "VS Code", "Playwright"],
     password: "false"
 }
 
